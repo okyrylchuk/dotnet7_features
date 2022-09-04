25e896548e21f035f53c0a3912c0c90aee4f01ba
Oleg Kyrylchuk <kyrylchuk.oleg@gmail.com>
2022-09-04 13:05:14 +0200

    Add GenericAttributes sample

---
+++ C# 11 features/GenericAttributes/GenericAttributes/Program.cs
@@ -0,0 +1,12 @@
+﻿// https://twitter.com/okyrylchuk/status/1552351944269828099
+
+class MyType { }
+
+class GenericAttribute<T> : Attribute
+    where T: MyType 
+{
+    private T _type;
+}
+
+[Generic<MyType>]
+class MyClass { }
